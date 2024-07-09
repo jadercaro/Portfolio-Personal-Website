46ab20057ab11cdc9e966329dbac4a0d69f6806f
jadercaro <jjadercaro192@gmail.com>
2024-07-09 16:32:04 +0000

    added post

--- Backend/django_portfolio/settings.py
+++ Backend/django_portfolio/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'django_portfolio.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 
 # Database
--- Backend/portafolio/views.py
+++ Backend/portafolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all() #trae todos los objetos de la base de datos
+    return render(request, 'home.html', {'projects': projects})
