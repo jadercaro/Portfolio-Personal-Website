a47c9c6ee956f9d638508096381298c729f49f05
jadercaro <jjadercaro192@gmail.com>
2024-07-20 14:33:19 -0500

    older version

--- Backend/portafolio/models.py
+++ Backend/portafolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField
 
+
 class project(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=250)
